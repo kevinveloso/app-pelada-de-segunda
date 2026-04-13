519d9baea3cc3e88758f6b52adf25e48b7e0b265
Kevin Veloso <kevinveloso@gmail.com>
2026-04-13 14:04:14 +0100

    + Fixing Controller annotations

--- src/main/java/com/peladadesegunda/app/controller/MatchController.java
+++ src/main/java/com/peladadesegunda/app/controller/MatchController.java
@@ -5,12 +5,11 @@ import com.peladadesegunda.app.service.MatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/match")
 public class MatchController {
 
